55a3714784f5762943ba9df4c05d2ed54941abe0
Josh Gavant <joshgavant@gmail.com>
2024-08-26 12:32:40 -0500

    add structured users

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,17 @@ const requestCounter = new prom.Counter({
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   requestCounter.inc({code: 200});
-  res.send('respond with a Users resource');
+  res.send([
+    { "name": "Josh",
+      "id": 1
+    },
+    { "name": "Jim",
+      "id": 2
+    },
+    { "name": "Jack",
+      "id": 3
+    }
+  ]);
 });
 
 module.exports = router;
